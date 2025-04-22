9d9d28beea30de00943d1a2f756cc8784a2f0883
Bethwelkipruto <bethwel.c7@gmail.com>
2025-04-22 09:13:33 +0300

    Update script.js
    remove and from my java script

--- src/script.js
+++ src/script.js
@@ -13,7 +13,7 @@ form.addEventListener("submit", function (e) {
     const year = parseInt(document.getElementById("year").value);
     const gender = document.querySelector('input[name="gender"]:checked').value;
 
-    // Create date and 
+    // Create date 
     const birthDate = new Date(year, month, day);
 
     if (
